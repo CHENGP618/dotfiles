5471673e74b09aa062df85f0f953e6f44b59e459
CHENG618 <chengp618@gmail.com>
2016-03-17 13:53:16 +0100

    add the configuration of rbenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,34 @@
-set default_user "paulirish"
-set default_machine "paulirish-macbookair2"
+# The fish folder should be linked in ~/.config/fish/
+# Also in ~/.config/fish/functions
+
+# Install on os x :
+# add fish to accepted shells
+# echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+# chsh -s /usr/local/bin/fish
 
 
-source ~/.config/fish/path.fish
-source ~/.config/fish/aliases.fish
-source ~/.config/fish/chpwd.fish
+# http://hackercodex.com/guide/install-fish-shell-mac-ubuntu/#basic-configuration
+# set -Ux EDITOR atom
+echo "Fish config loaded"
+
+set -g -x fish_greeting ''
+abbr -a g "git"
+abbr -a h "heroku"
+complete -c c  -a "(__fish_complete_directories (~/RubyOnRails/))"
+set -gx PATH  /usr/local/heroku/bin $PATH
 
 
-# Completions
-function make_completion --argument-names alias command
-    echo "
-    function __alias_completion_$alias
-        set -l cmd (commandline -o)
-        set -e cmd[1]
-        complete -C\"$command \$cmd\"
-    end
-    " | .
-    complete -c $alias -a "(__alias_completion_$alias)"
-end
+set rbenv_bin "$HOME/.rbenv/bin"
+set brew_rbenv "/usr/local/var/rbenv/shims"
+set local_bin "./bin"
+set -gx PATH $local_bin $brew_rbenv $rbenv_bin $PATH
 
-make_completion g 'git'
+# rbenv exec gem install bundler solved it ?
+# user hombrew for rbenv and ruby build
 
-
+### Ruby (rbenv) ###
+set -gx RBENV_ROOT /usr/local/var/rbenv
+rbenv rehash >/dev/null ^&1
 
 
 # Readline colors
@@ -47,39 +54,3 @@ set -g fish_color_valid_path --underline
 
 set -g fish_color_dimmed 555
 set -g fish_color_separator 999
-
-# Git prompt status
-set -g __fish_git_prompt_showdirtystate 'yes'
-set -g __fish_git_prompt_showupstream auto
-
-
-# Status Chars
-#set __fish_git_prompt_char_dirtystate '*'
-set __fish_git_prompt_char_upstream_equal ''
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_dirtystate 'red'
-
-set __fish_git_prompt_color_upstream_ahead ffb90f
-set __fish_git_prompt_color_upstream_behind blue
-
-# Local prompt customization
-set -e fish_greeting
-
-
-set -g fish_pager_color_completion normal
-set -g fish_pager_color_description 555 yellow
-set -g fish_pager_color_prefix cyan
-set -g fish_pager_color_progress cyan
-
-
-# highlighting inside manpages and elsewhere
-set -gx LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
-set -gx LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
-set -gx LESS_TERMCAP_me \e'[0m'           # end mode
-set -gx LESS_TERMCAP_se \e'[0m'           # end standout-mode
-set -gx LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
-set -gx LESS_TERMCAP_ue \e'[0m'           # end underline
-set -gx LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
-
